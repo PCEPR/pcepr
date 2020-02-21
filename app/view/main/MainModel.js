49fb8edb0431d07b9e69a9c915297c4eaab11cad
yadber <yadber58@gmail.com>
2020-02-21 09:16:39 -0800

    first commit

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('PCEPR.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'PCEPR'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
